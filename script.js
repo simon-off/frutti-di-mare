47a99b0b65ab577754452073dfa47bff92049def
simon <backstrom94+github@gmail.com>
2022-11-01 16:07:39 +0100

    created fetch function and changed api. Fish instead of fruits

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-function fetchData() {}
+async function fetchData() {
+  const result = await fetch("https://www.fishwatch.gov/api/species/red-snapper");
+  console.log(result);
+  const data = await result.json();
+  console.log(data);
+}
+
+fetchData();
