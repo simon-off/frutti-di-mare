2683db94b18ee33a9fec004c8dd83dce48a70743
Simon <backstrom94+github@gmail.com>
2022-11-03 23:11:23 +0100

    Got a bit carried away. Made lots of changes. Update styles all over. Added images. Added some new functions to the script. You can now add/remove fishes to your collection. Your collection also saves to localStorage.
    Still lots to do but great progress!

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
+//===========================================================//
+//+++ GLOBAL VARIABLES +++||---------------------------------//
+//===========================================================//
+
 const resultsEl = document.querySelector(".results");
+const collectionEl = document.querySelector(".collection");
 const searchField = document.querySelector("#search");
 const showSelect = document.querySelector("#show-amount");
 const sortSelect = document.querySelector("#sort-by");
@@ -6,6 +11,48 @@ const sortSelect = document.querySelector("#sort-by");
 const apiURL = "https://www.fishwatch.gov/api/species";
 
 let fishCache;
+let fishCollection = localStorage.getItem("fishes")
+  ? JSON.parse(localStorage.getItem("fishes"))
+  : [];
+
+//===========================================================//
+//+++ COMMON FUNCTIONS +++||---------------------------------//
+//===========================================================//
+
+// TODO: Lägg till mer stats
+function createMarkup(fish) {
+  const markup = document.createElement("article");
+  markup.classList.add("item");
+  markup.innerHTML = `
+  <div class="item__head">
+    <div class="item__head__expand">
+      <h3>${fish["Species Name"]}</h3>
+    </div>
+  </div>
+  <div class="item__body">
+    <img src="${fish["Species Illustration Photo"].src}" alt="${fish["Species Illustration Photo"].alt}">
+    <div class="item__facts">
+      <p>Calories: ${fish["Calories"]} - Fat: ${fish["Fat, Total"]}g</p>
+    </div>
+  </div>
+  `;
+  const expand = markup.querySelector(".item__head__expand");
+
+  // Expand button
+  const expandButton = document.createElement("input");
+  expandButton.classList.add("expand");
+  expandButton.type = "image";
+  expandButton.src = "../svg/arrow.svg";
+  expandButton.alt = "Expand result";
+  expand.addEventListener("click", () => markup.classList.toggle("expanded"));
+  expand.append(expandButton);
+
+  return markup;
+}
+
+//===========================================================//
+//+++ RESULT FUNCTIONS +++||---------------------------------//
+//===========================================================//
 
 function sortFishes(fishes) {
   const sortValue = sortSelect.value;
@@ -19,42 +66,12 @@ function sortFishes(fishes) {
       if (valueA > valueB) return 1;
     });
   }
-  // sort by fat
-  if (sortValue === "Fat, Total") {
-    return fishes.sort((a, b) => {
-      const valueA = a[sortValue].split(" ")[0];
-      const valueB = b[sortValue].split(" ")[0];
-      return valueB - valueA;
-    });
-  }
-  // sort by calories
+  // sort by calories or fat
   return fishes.sort((a, b) => b[sortValue] - a[sortValue]);
 }
 
 function createResultMarkup(fish) {
-  const markup = document.createElement("article");
-  markup.classList.add("result");
-  markup.innerHTML = `
-    <div class="result__head">
-      <div class="result__head__expand">
-        <h3>${fish["Species Name"]}</h3>
-      </div>
-    </div>
-    <div class="result__body">
-      <p>Calories: ${fish["Calories"]}</p>
-      <p>Fat: ${fish["Fat, Total"]}</p>
-    </div>
-  `;
-  const expand = markup.querySelector(".result__head__expand");
-
-  // Expand button
-  const expandButton = document.createElement("input");
-  expandButton.classList.add("expand");
-  expandButton.type = "image";
-  expandButton.src = "../svg/arrow.svg";
-  expandButton.alt = "Expand result";
-  expand.addEventListener("click", () => markup.classList.add("expanded"));
-  expand.append(expandButton);
+  const markup = createMarkup(fish);
 
   // Add button
   const addButton = document.createElement("input");
@@ -62,11 +79,11 @@ function createResultMarkup(fish) {
   addButton.type = "image";
   addButton.src = "../svg/plus.svg";
   addButton.alt = "Add result to collection";
-  addButton.addEventListener("click", () => addResult(fish));
-  markup.querySelector(".result__head").append(addButton);
+  addButton.addEventListener("click", () => saveFish(fish));
+  markup.querySelector(".item__head").append(addButton);
 
   //
-  fish.markup = markup;
+  fish.resultMarkup = markup;
 }
 
 function updateResults(newData) {
@@ -87,18 +104,97 @@ function updateResults(newData) {
 
   for (let fish of fishes) {
     // Create markup for fish in results if not present
-    if (!fish.markup) createResultMarkup(fish);
-    resultsEl.append(fish.markup);
+    if (!fish.resultMarkup) createResultMarkup(fish);
+    resultsEl.append(fish.resultMarkup);
   }
 }
 
+function saveFish(fish) {
+  if (!fishCollection.includes(fish)) {
+    fish.amount = 1;
+    fishCollection.push(fish);
+  } else {
+    fish.amount += 1;
+  }
+
+  updateCollection();
+}
+
+//===========================================================//
+//+++ COLLECTION FUNCTIONS +++||-----------------------------//
+//===========================================================//
+
+function updateCollectionStats() {
+  // TODO: Uppdatera stats för alla fiskar va.
+  console.log("hello");
+}
+
+function createCollectionMarkup(fish) {
+  const markup = createMarkup(fish);
+
+  // Add amount to header
+  const amount = document.createElement("span");
+  amount.id = "amount-span";
+  markup.querySelector("h3").prepend(amount);
+
+  // Remove button
+  const removeButton = document.createElement("input");
+  removeButton.classList.add("remove");
+  removeButton.type = "image";
+  removeButton.src = "../svg/cross.svg";
+  removeButton.alt = "Remove fish from collection";
+  removeButton.addEventListener("click", () => removeFish(fish));
+  markup.querySelector(".item__head").append(removeButton);
+
+  //
+  fish.collectionMarkup = markup;
+}
+
+function updateCollection() {
+  localStorage.setItem("fishes", JSON.stringify(fishCollection));
+  updateCollectionStats();
+  collectionEl.innerHTML = "";
+
+  for (let fish of fishCollection) {
+    // Create markup for fish in collection if not present
+    createCollectionMarkup(fish);
+
+    const amountSpan = fish.collectionMarkup.querySelector("#amount-span");
+    amountSpan.innerText = fish.amount + " - ";
+
+    collectionEl.append(fish.collectionMarkup);
+  }
+}
+
+function removeFish(fish) {
+  fish.amount--;
+  if (fish.amount < 1) {
+    fishCollection.splice(fishCollection.indexOf(fish), 1);
+  }
+  updateCollection();
+}
+
+//===========================================================//
+//+++ FETCH FUNCTIONS +++||----------------------------------//
+//===========================================================//
+
 function fetchSuccess(data) {
-  // Make sure there's no null values
+  // Remove duplicates
+  const species = "Species Name";
+  data = data.filter(
+    (el, index, self) => index === self.findIndex((t) => t[species] === el[species])
+  );
+
   for (let fish of data) {
+    // Remove null values
     if (!fish["Fat, Total"]) fish["Fat, Total"] = "0";
     if (!fish["Calories"]) fish["Calories"] = "0";
+    // Remove "g" from fat
+    fish["Fat, Total"] = fish["Fat, Total"].split(" ")[0];
   }
+
   updateResults(data);
+  updateCollection();
   fishCache = data;
 }
 
@@ -112,6 +208,10 @@ async function fetchData(url) {
   const data = await result.json();
   fetchSuccess(data);
 }
+
+//===========================================================//
+//+++ EVENT LISTENERS +++||----------------------------------//
+//===========================================================//
 
 searchField.addEventListener("input", () => updateResults());
 showSelect.addEventListener("input", () => updateResults());
